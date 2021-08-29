e2e31d3e6f3cc1a569be3248a44761e9cc71f6e6
Tikhon <tihonmars@gmail.com>
2021-08-29 17:06:49 +0300

    сохранить состоятние свитча

--- app/src/main/java/com/atdev/githubproject/profile/fragments/ProfileFragment.kt
+++ app/src/main/java/com/atdev/githubproject/profile/fragments/ProfileFragment.kt
@@ -2,6 +2,7 @@ package com.atdev.githubproject.profile.fragments
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,13 +12,12 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.atdev.githubproject.MainActivity
 import com.atdev.githubproject.R
-import com.atdev.githubproject.profile.auth.data.LoginRepository
-import com.atdev.githubproject.profile.auth.activity.LoginActivity
 import com.atdev.githubproject.databinding.FragmentProfileBinding
+import com.atdev.githubproject.profile.auth.activity.LoginActivity
+import com.atdev.githubproject.profile.auth.data.LoginRepository
 import com.atdev.githubproject.profile.viewmodel.ProfileViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
-
 
 @AndroidEntryPoint
 class ProfileFragment : Fragment() {
@@ -26,12 +26,13 @@ class ProfileFragment : Fragment() {
     lateinit var loginRepository: LoginRepository
 
     private lateinit var binding: FragmentProfileBinding
-    private val profileViewModel: ProfileViewModel by viewModels()
+    private val viewModel: ProfileViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
         binding = DataBindingUtil.inflate(
             inflater,
             R.layout.fragment_profile,
@@ -40,20 +41,30 @@ class ProfileFragment : Fragment() {
         )
         (requireActivity() as MainActivity).invalidateOptionsMenu()
 
-        binding.viewModel = profileViewModel
-
         setupObserver()
 
-        binding.darkModeSwitch.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-            else AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-        }
+        binding.darkModeSwitch.isChecked = viewModel.getValue()
 
+        binding.viewModel = viewModel
+
+        Log.i("TEST_VM_FRAGEMENT",viewModel.getValue().toString())
+
+        binding.darkModeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+        }
         return binding.root
     }
 
+    override fun onPause() {
+        super.onPause()
+        viewModel.setQuery(binding.darkModeSwitch.isChecked)
+        Log.i("TEST_PAUSE",binding.darkModeSwitch.isChecked.toString())
+    }
+
     private fun setupObserver() {
-        profileViewModel.logoutStatus.observe(viewLifecycleOwner, {
+        viewModel.logoutStatus.observe(viewLifecycleOwner, {
             if (!loginRepository.isLoggedIn) {
                 startActivity(Intent(requireContext(), LoginActivity::class.java))
             }
--- app/src/main/java/com/atdev/githubproject/profile/viewmodel/ProfileViewModel.kt
+++ app/src/main/java/com/atdev/githubproject/profile/viewmodel/ProfileViewModel.kt
@@ -1,5 +1,6 @@
 package com.atdev.githubproject.profile.viewmodel
 
+import android.util.Log
 import android.view.View
 import androidx.lifecycle.*
 import com.atdev.githubproject.profile.auth.data.LoginRepository
@@ -8,10 +9,22 @@ import javax.inject.Inject
 
 @HiltViewModel
 class ProfileViewModel @Inject constructor(
+    private val state : SavedStateHandle,
     private val loginRepository: LoginRepository,
     ) : ViewModel() {
 
-    var switchDarkMode =  MutableLiveData<Boolean>(false)
+    private val savedStateHandle = state
+
+    private val testvar : MutableLiveData<Boolean> = savedStateHandle.getLiveData("TEST")
+
+    fun getValue(): Boolean {
+        return testvar.value ?: false
+    }
+
+    fun setQuery(value: Boolean) {
+        savedStateHandle.set("TEST", value)
+        Log.i("TEST_VM_SET_QUERY",value.toString())
+    }
 
     var username = MutableLiveData<String>(loginRepository.user?.displayName)
     var secondName = MutableLiveData<String>()
